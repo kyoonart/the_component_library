8edc802b56d99ff696ebee042e92ee3622fdfc09
pengtaoa <1564328778@qq.com>
2020-09-12 10:14:59 +0800

    fix:style bug

--- src/components/Alert/alert.test.tsx
+++ src/components/Alert/alert.test.tsx
@@ -7,39 +7,47 @@ const successProps = {
   title: "标题",
   description: "描述",
 };
-describe('test Alert component', () => {
-  it('should render the correct default alert', () => {
-    const { getByText, container } = render(<Alert></Alert>)
-    expect(container.querySelector('.alert')).toBeInTheDocument()
+describe("test Alert component", () => {
+  it("should render the correct default alert", () => {
+    const { getByText, container } = render(<Alert></Alert>);
+    expect(container.querySelector(".alert")).toBeInTheDocument();
 
     // 测试类型
-    expect(container.querySelector(".alert")).toHaveClass("alert alert-default")
-    expect(getByText('默认描述')).toBeInTheDocument()
+    expect(container.querySelector(".alert")).toHaveClass(
+      "alert alert-default"
+    );
+    expect(getByText("默认描述")).toBeInTheDocument();
 
     // 测试icon
-    expect(container.querySelector(".alert-icon")).not.toBeInTheDocument()
-  })
+    expect(container.querySelector(".alert-icon")).not.toBeInTheDocument();
+  });
 
-  it('should render the correct component based on different props', async () => {
-    const { getByText, container } = render(<Alert {...successProps} type='success'></Alert>)
-    expect(container.querySelector('.alert')).toBeInTheDocument()
+  it("should render the correct component based on different props", async () => {
+    const { getByText, container } = render(
+      <Alert {...successProps} type="success"></Alert>
+    );
+    expect(container.querySelector(".alert")).toBeInTheDocument();
 
     // 测试类型
-    expect(container.querySelector(".alert")).toHaveClass("alert alert-success")
+    expect(container.querySelector(".alert")).toHaveClass(
+      "alert alert-success"
+    );
 
     // 测试标题
-    expect(getByText('标题')).toHaveClass('alert-title')
+    expect(getByText("标题")).toHaveClass("alert-title");
 
     // 测试描述
-    expect(getByText('描述')).toHaveClass('alert-desc')
+    expect(getByText("描述")).toHaveClass("alert-desc");
 
     // 测试icon是否存在
-    expect(container.querySelector('.alert-close')).toBeInTheDocument()
-    const closeIcon = container.querySelector('.alert-close') as HTMLButtonElement
-    fireEvent.click(closeIcon)
-    expect(successProps.onClose).toHaveBeenCalled()
+    expect(container.querySelector(".alert-close")).toBeInTheDocument();
+    const closeIcon = container.querySelector(
+      ".alert-close"
+    ) as HTMLButtonElement;
+    fireEvent.click(closeIcon);
+    expect(successProps.onClose).toHaveBeenCalled();
     await wait(() => {
-      expect(container.querySelector('.alert')).not.toBeInTheDocument()
+      expect(container.querySelector(".alert")).not.toBeInTheDocument();
     });
-  })
-}
+  });
+});
